e99d3fea6067be05075d02835738db087b9aa64d
AlexZhidkov <AlexZhidkov@users.noreply.github.com>
2014-07-23 15:24:34 +0800

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -7,80 +7,104 @@ function ($scope, $window) {
     $scope.team1 = {
         'name': 'East',
         'count': 0,
-        'player1': 'Person11',
-        'player2': 'Person12',
+        'player1': 'East1',
+        'player2': 'East2',
         'lastServed': ''
     };
     $scope.team2 = {
         'name': 'West',
         'count': 0,
-        'player1': 'Person21',
-        'player2': 'Person22',
+        'player1': 'West1',
+        'player2': 'West2',
         'lastServed': ''
     };
-    $scope.teamTemp = {
-        'name': '',
-        'count': 0,
-        'player1': '',
-        'player2': '',
-        'lastServed': ''
-    };
+
+    $scope.history = [];
 
     $scope.Math = $window.Math;
 
     $scope.teamLeft = $scope.team1;
     $scope.teamRight = $scope.team2;
 
-    $scope.lastScored = "";
-    $scope.serveLeft1 = "O";
-    $scope.nextServer = "teamLeft.player1";
+    $scope.lastScored = "teamLeft";
+    $scope.nextServer = "player1";
+
+    var isSwitched = true;
+
+    $scope.historyAdd = function () {
+        isSwitched = false;
+        $scope.history.push({
+            teamLeft: angular.copy($scope.teamLeft),
+            teamRight: angular.copy($scope.teamRight),
+            lastScored: $scope.lastScored,
+            nextToServe: $scope.nextServer
+        });
+    };
 
     $scope.pointLeft = function () {
         $scope.teamLeft.count = $scope.teamLeft.count + 1;
         if ($scope.lastScored == "teamRight") {
-            $scope.teamRight.lastServed = nextServer;
-            if ($scope.teamLeft.lastServed == "teamLeft.player1") {
-                nextServer = "teamLeft.player2"
+            $scope.teamRight.lastServed = $scope.nextServer;
+            if ($scope.teamLeft.lastServed == "player1") {
+                $scope.nextServer = "player2"
             } else {
-                nextServer = "teamLeft.player1"
+                $scope.nextServer = "player1"
             }
         }
         $scope.lastScored = "teamLeft";
+        $scope.historyAdd();
     };
 
     $scope.pointRight = function () {
         $scope.teamRight.count = $scope.teamRight.count + 1;
         if ($scope.lastScored == "teamLeft") {
-            $scope.teamLeft.lastServed = nextServer;
-            if ($scope.teamRight.lastServed == "teamRight.player1") {
-                nextServer = "teamRight.player2"
+            $scope.teamLeft.lastServed = $scope.nextServer;
+            if ($scope.teamRight.lastServed == "player1") {
+                $scope.nextServer = "player2"
             } else {
-                nextServer = "teamRight.player1"
+                $scope.nextServer = "player1"
             }
         }
         $scope.lastScored = "teamRight";
+        $scope.historyAdd();
     };
 
     $scope.undo = function () {
-        if ($scope.lastScored == "teamLeft") {
-            $scope.teamLeft.count = $scope.teamLeft.count - 1;
+        $scope.history.splice($scope.history.length - 1, 1);
+
+        var point = angular.copy($scope.history[$scope.history.length - 1]);
+
+        $scope.teamLeft = point.teamLeft;
+        $scope.teamRight = point.teamRight;
+        $scope.lastScored = point.lastScored;
+        $scope.nextServer = point.nextToServe;
+
+        if ($scope.timeToSwitch()) {
+            $scope.switchSides();
         }
-        else if ($scope.lastScored == "teamRight") {
-            $scope.teamRight.count = $scope.teamRight.count - 1;
-        }
-        $scope.lastScored = "";
     };
 
     $scope.switchSides = function () {
-        $scope.teamTemp = $scope.teamLeft;
+        teamTemp = $scope.teamLeft;
         $scope.teamLeft = $scope.teamRight;
-        $scope.teamRight = $scope.teamTemp;
-        $scope.lastScored = "";
+        $scope.teamRight = teamTemp;
+        if ($scope.lastScored == "teamLeft") {
+            $scope.lastScored = "teamRight";
+        } else {
+            $scope.lastScored = "teamLeft";
+        }
+        isSwitched = true;
     };
 
     $scope.timeToSwitch = function () {
-        value = ($scope.team1.count + $scope.team2.count) / $scope.switchOnPoints;
+        if (isSwitched) {
+            return false;
+        }
+        value = ($scope.teamLeft.count + $scope.teamRight.count) / $scope.switchOnPoints;
         return $scope.lastScored && value == $scope.Math.round(value);
     }
+
+    $scope.historyAdd();
+    isSwitched = true;
 
 }]);
